ebd49d372a235803cbf2eac67657bff447879855
MDIOUF31 <141971397+MDIOUF31@users.noreply.github.com>
2023-08-23 13:28:16 +0000

    Add files via upload

---
+++ SQLQuery5.sql
@@ -0,0 +1,8 @@
+INSERT INTO CLIENT (client_id, nom, adresse)
+VALUES( 'client_id', 'nom', 'adresse')
+
+INSERT INTO PRODUIT (produit_id, nom, prix)
+VALUES('produit_id', 'nom', 'prix')
+
+INSERT INTO COMMANDE (commande_id, quantite, datecommande)
+VALUES ('commande_id','quantite', 'datecommande')
